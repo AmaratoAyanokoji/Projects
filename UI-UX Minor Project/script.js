0784ba5799da8efc8f0fa7a630941cbfeae642c8
AmaratoAyanokoji <xghost579@gmail.com>
2023-07-21 14:30:32 +0530

    new commit

---
+++ UI-UX Minor Project/script.js
@@ -0,0 +1,33 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
+
+function changePage(page) {
+    window.location.href = page;
+}
+
+function scrollPage(item) {
+    window.location.href = item;
+}
+
+
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
